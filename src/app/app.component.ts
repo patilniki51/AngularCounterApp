400b9b5b0b009b0b8df5e53541a7bfc0b448f55b
Nikita <patilniki51@gmail.com>
2020-07-13 01:59:29 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'counter-app';
+  title = 'Counter App';
+  count: number = 0;
+
+  handleIncrease = () =>{
+    this.count = this.count + 1;
+  }
+
+  handleDecrease = () =>{
+    this.count = this.count - 1;
+  }
+
+  handleReset = () =>{
+    this.count = 0;
+  }
 }
